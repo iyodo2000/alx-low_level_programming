c26e9c1f899b44a4e298c26a6c137593079eae42
iyodo michael <iyodoiyodo@gmail.com>
2022-06-30 18:17:11 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+
+
+
+/** 
+ *main - Entry point*
+ *
+ * Description: 'the program's description
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf("with proper grammer, but the outcome ia a piece of art ,\n");
+printf("with proper grammar, but the outcome is a piece of art ,\n");
 return (0);
 }
