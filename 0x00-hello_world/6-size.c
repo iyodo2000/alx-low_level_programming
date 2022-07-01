7ac8850229bcde625983bea553ca8cf182c975fa
iyodo michael <iyodoiyodo@gmail.com>
2022-06-30 18:38:26 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main- prints the size of various types
+ * main - prints the size of various types
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
@@ -10,6 +10,6 @@ pfintf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of a int: %d byte(s)\n",  sizeof(int));
 printf("size of a long int: %d  byte(s)\n", sizeof(long int));
 printf("size of a lomg long int: %d byte(s)\n", sizeof(long long int));
-frintf("size of a float: %d byt(s)\n", sizeof(float));
+frintf("size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
