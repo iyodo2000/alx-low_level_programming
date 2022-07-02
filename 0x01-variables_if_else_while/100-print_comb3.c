5124dabcedd43fd3d2188b6021a4b2f10c832807
iyodo michael <iyodoiyodo@gmail.com>
2022-07-02 12:43:08 -0700

    done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,13 +19,16 @@ for (m = 49; m <= 57; m++)
 if (m > n)
 {
 putchar(n);
-putchar(m);if (n != 56 || m != 57)
+putchar(m);if (n != 56 || m != 57);
 {
 putchar(',');
 putchar(' ');
 }
+
 }
+
 }
+
 }
 putchar('\n');
 return (0);
