ab95505b1fc6899281e82c00b3773e444f29974c
iyodo michael <iyodoiyodo@gmail.com>
2022-07-06 14:11:38 -0700

    done

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -22,13 +22,13 @@ int main(void)
 
 
 	/*
-	 *while (i < 10)
+	 * while (i < 10)
 	 *
 	 * {
 	 *
-	 *Infinite loop - No increase of variable
+	 * Infinite loop - No increase of variable
 	 *
-	 *putchar(i);
+	 * putchar(i);
 	 *
          * }
 	 *
