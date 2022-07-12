408f06b9433b0703288aadb8ed1c0393e899f442
iyodo michael <iyodoiyodo@gmail.com>
2022-07-12 10:49:00 -0700

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,18 +10,15 @@
 
 void print_rev(char *s)
 {
-
-	int i;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	for (i = i - 1 ; i >= 0; i--)
-	{
-		_putchar (s[i]);
-	}
-
-	_putchar ('\n');
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+for (i = i - 1 ; i >= 0; i--)
+{
+_putchar (s[i]);
+}
+_putchar ('\n');
 }
